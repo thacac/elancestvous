32e26b2a199a20f3bb13e86b3719bff2c4da590c
thacac <cedric@thecacproject.com>
2025-12-29 16:48:29 +0100

    fix:deploy chain updated

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Vérification de sécurité : si VPS_USR est vide, on arrête tout proprement
+if [ -z "$VPS_USR" ]; then
+  echo "Erreur : La variable VPS_USR est vide !"
+  exit 1
+fi
+
 # On se place à la racine du projet là où est le docker-compose.yaml
 cd /home/$VPS_USR/elancestvous
 
